13118a67c7301cd145cc157658f681e6911fa3e8
ahmedabdalla67 <125463647+ahmedabdalla67@users.noreply.github.com>
2023-10-10 12:41:22 +0300

    change theme and build note view

---
+++ lib/views/notes_app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Notes',
+              style: TextStyle(fontSize: 22),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.black26,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Icon(
+                  Icons.search,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
