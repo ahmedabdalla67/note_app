348f6660c0bdeb04c5f796da6322794cf2717709
ahmedabdalla67 <125463647+ahmedabdalla67@users.noreply.github.com>
2023-10-10 19:11:30 +0300

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
-import 'package:note_app/views/widgets/custom_note_item.dart';
+import 'package:note_app/views/widgets/note_list_view.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
@@ -16,10 +20,8 @@ class NotesView extends StatelessWidget {
               height: 50,
             ),
             CustomAppBar(),
-            SizedBox(
-              height: 20,
-            ),
-            CustomNoteItem(),
+            SizedBox(height: 8),
+            NoteListView(),
           ],
         ),
       ),
---
+++ lib/views/widgets/note_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_note_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          return const CustomNoteItem();
+        },
+        itemCount: 10,
+        physics: const BouncingScrollPhysics(),
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            height: 8,
+          );
+        },
+      ),
+    );
+  }
+}
